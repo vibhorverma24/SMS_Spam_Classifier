f30090ef8bcda7712fc7d2ceaeb96d4e7f527577
vibhorverma24 <vibhorverma8790@gmail.com>
2025-09-14 15:01:50 +0530

    changes in nltk

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
+
 nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
+
 ps = PorterStemmer()
 
 def transform_text(text):
